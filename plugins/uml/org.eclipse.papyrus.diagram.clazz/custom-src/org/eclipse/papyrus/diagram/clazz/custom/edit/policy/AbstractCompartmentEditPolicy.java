51262c2be8c7d37e46add3ec13d07783071244d8
adaussy <adaussy@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-06-19 13:40:46 +0000

    382605: [ClassDiagram] NPE when DND of Property , NestedClass and Operation  with owner in a package https://bugs.eclipse.org/bugs/show_bug.cgi?id=382605

--- plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/edit/policy/AbstractCompartmentEditPolicy.java
+++ plugins/uml/org.eclipse.papyrus.diagram.clazz/custom-src/org/eclipse/papyrus/diagram/clazz/custom/edit/policy/AbstractCompartmentEditPolicy.java
@@ -17,6 +17,7 @@ package org.eclipse.papyrus.diagram.clazz.custom.edit.policy;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edit.command.MoveCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
@@ -25,13 +26,18 @@ import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.requests.CreateRequest;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.FlowLayoutEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableEditPolicyEx;
+import org.eclipse.gmf.runtime.diagram.ui.requests.EditCommandRequestWrapper;
 import org.eclipse.gmf.runtime.emf.commands.core.commands.RepositionEObjectCommand;
-import org.eclipse.gmf.runtime.emf.type.core.commands.MoveElementsCommand;
-import org.eclipse.gmf.runtime.emf.type.core.requests.MoveRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.papyrus.commands.wrappers.EMFtoGEFCommandWrapper;
 import org.eclipse.papyrus.diagram.clazz.custom.edit.commands.CompartmentRepositionEObjectCommand;
+import org.eclipse.papyrus.diagram.clazz.part.UMLVisualIDRegistry;
+import org.eclipse.papyrus.diagram.clazz.providers.UMLElementTypes;
 
 
 /**
@@ -43,23 +49,50 @@ public abstract class AbstractCompartmentEditPolicy extends FlowLayoutEditPolicy
 	private EStructuralFeature feature = null;
 
 	protected Command createAddCommand(EditPart child, EditPart after) {
-		int index = getHost().getChildren().indexOf(after);
-		TransactionalEditingDomain editingDomain = ((IGraphicalEditPart)getHost()).getEditingDomain();
 
+		/*
+		 * This edit policy only handle intra child movement
+		 */
+		
 
-
-		EObject toReparentObject = ((IGraphicalEditPart)child).resolveSemanticElement();
-		IGraphicalEditPart newPartContianer = getCorrectCompartment(toReparentObject);
-
-		if(newPartContianer == null) {
+		boolean isCorrectCompartment = isCorrectCompartment(((IGraphicalEditPart)child).getNotationView());
+		if (!isCorrectCompartment){
 			return org.eclipse.gef.commands.UnexecutableCommand.INSTANCE;
 		}
-		View container = newPartContianer.getNotationView();
-		View childView = (View)child.getModel();
+		
+		if (child != null &&  after != null && child.getParent().equals(after.getParent())){
+			
+			int index = getHost().getChildren().indexOf(after);
+			TransactionalEditingDomain editingDomain = ((IGraphicalEditPart)getHost()).getEditingDomain();
+			
+			
+			
+			EObject toReparentObject = ((IGraphicalEditPart)child).resolveSemanticElement();
+			
+			IGraphicalEditPart newPartContianer = getCorrectCompartment(toReparentObject);
+			
+			if(newPartContianer == null) {
+				return org.eclipse.gef.commands.UnexecutableCommand.INSTANCE;
+			}
+			View container = newPartContianer.getNotationView();
+			View childView = (View)child.getModel();
+			
+			MoveCommand command = new MoveCommand(editingDomain, container, NotationPackage.Literals.VIEW__PERSISTED_CHILDREN, childView, index);
+			return new EMFtoGEFCommandWrapper(command);
+		}
+		return null;
+	}
 
-		MoveElementsCommand mvCmd = new MoveElementsCommand(new MoveRequest(container, childView));
-//		MoveCommand command = new MoveCommand(editingDomain, container, NotationPackage.Literals.VIEW__PERSISTED_CHILDREN, childView, index);
-		return new ICommandProxy(mvCmd);
+	private boolean isCorrectCompartment(View viewReparentObject) {
+		
+		EditPolicy policy = getHost().getEditPolicy(EditPolicyRoles.SEMANTIC_ROLE);
+		
+		CreateElementRequest createRequest = new CreateElementRequest(UMLElementTypes.getElementType(UMLVisualIDRegistry.getVisualID(viewReparentObject.getType())));
+		if (policy != null){
+			Command cmd = policy.getCommand(new EditCommandRequestWrapper(createRequest));
+			return cmd != null && cmd.canExecute();
+		}
+		return false;
 	}
 
 	protected abstract IGraphicalEditPart getCorrectCompartment(EObject toReparentObject);
@@ -69,37 +102,63 @@ public abstract class AbstractCompartmentEditPolicy extends FlowLayoutEditPolicy
 		policy.setResizeDirections(0);
 		return policy;
 	}
+	
+	@Override
+	protected Command getMoveChildrenCommand(Request request) {
+		
+		Command cmd = super.getMoveChildrenCommand(request);
+		if (cmd != null && cmd.canExecute()){
+			return cmd;
+		}
+		return null;
+	}
+	
+	@Override
+	protected Command getAddCommand(Request req) {
+
+		Command cmd =super.getAddCommand(req);
+		if (cmd != null && cmd.canExecute()){
+			return cmd;
+		}
+		return null;
+	}
 
 	protected Command createMoveChildCommand(EditPart child, EditPart after) {
-
-
-		int newIndex;
-		int displacement;
-
-		int childIndex = getHost().getChildren().indexOf(child);
-		int afterIndex = getHost().getChildren().indexOf(after);
-
-		if(afterIndex == -1) {
-			newIndex = getHost().getChildren().size() - 1;
-			displacement = newIndex - childIndex;
-		} else {
-			newIndex = afterIndex;
-			displacement = afterIndex - childIndex;
-			if(childIndex <= afterIndex) {
-				newIndex--;
-				displacement--;
+		
+		/*
+		 * This edit policy only handle intra child movement
+		 */
+		
+		if (child != null && after != null && child.getParent().equals(after.getParent())){
+			
+			int newIndex;
+			int displacement;
+			
+			int childIndex = getHost().getChildren().indexOf(child);
+			int afterIndex = getHost().getChildren().indexOf(after);
+			
+			if(afterIndex == -1) {
+				newIndex = getHost().getChildren().size() - 1;
+				displacement = newIndex - childIndex;
+			} else {
+				newIndex = afterIndex;
+				displacement = afterIndex - childIndex;
+				if(childIndex <= afterIndex) {
+					newIndex--;
+					displacement--;
+				}
 			}
+			
+			
+			TransactionalEditingDomain editingDomain = ((IGraphicalEditPart)getHost()).getEditingDomain();
+			
+			RepositionEObjectCommand command = new CompartmentRepositionEObjectCommand(child, editingDomain, "", (EList)((View)child.getParent().getModel()).getElement().eGet(feature), ((View)child.getModel()).getElement(), displacement, newIndex);
+			
+			eraseLayoutTargetFeedback(null);
+			
+			return new ICommandProxy(command);
 		}
-
-
-		TransactionalEditingDomain editingDomain = ((IGraphicalEditPart)getHost()).getEditingDomain();
-
-		RepositionEObjectCommand command = new CompartmentRepositionEObjectCommand(child, editingDomain, "", (EList)((View)child.getParent().getModel()).getElement().eGet(feature), ((View)child.getModel()).getElement(), displacement, newIndex);
-
-		//TODO ev. reintroduce target feedback (actual problem: line is not deleted after dropping)
-		eraseLayoutTargetFeedback(null);
-
-		return new ICommandProxy(command);
+		return null;
 	}
 
 	protected Command getCreateCommand(CreateRequest request) {
