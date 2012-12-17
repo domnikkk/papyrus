e3334e4bb3ff9ff7a5ea76d91776a1841e3eb6d1
mvelten <mvelten@d68fad7f-2b57-0410-97a2-f40a37629c47>
2012-12-17 10:27:43 +0000

    order diagrams by type and alphabetically in the model explorer (eyy specific)

--- plugins/uml/org.eclipse.papyrus.uml.modelexplorer/src/org/eclipse/papyrus/uml/modelexplorer/queries/GetContainedDiagrams.java
+++ plugins/uml/org.eclipse.papyrus.uml.modelexplorer/src/org/eclipse/papyrus/uml/modelexplorer/queries/GetContainedDiagrams.java
@@ -8,11 +8,18 @@
  *  
  *  Contributors:
  *  Atos - Initial API and implementation
+ *  
+ *  Anass RADOUANI (Atos) anass.radouani@atos.net - Order Diagrams by type and by name
  * 
  */
 package org.eclipse.papyrus.uml.modelexplorer.queries;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.emf.facet.infra.query.core.exception.ModelQueryExecutionException;
 import org.eclipse.emf.facet.infra.query.core.java.IJavaModelQuery;
@@ -25,7 +32,63 @@ import org.eclipse.uml2.uml.Element;
 /** Get the collection of all contained diagrams */
 public class GetContainedDiagrams extends AbstractEditorContainerQuery implements IJavaModelQuery<Element, Collection<org.eclipse.gmf.runtime.notation.Diagram>> {
 
+	public static Pattern PATTERN_NUMBERS = Pattern.compile("(.*(?:\\D|^))(\\d+)");
+
+
 	public Collection<Diagram> evaluate(final Element context, final ParameterValueList parameterValues) throws ModelQueryExecutionException {
-		return NotationUtils.getAssociatedDiagrams(context);
+		List<Diagram> associatedDiagrams = NotationUtils.getAssociatedDiagrams(context);
+
+
+		Collections.sort(associatedDiagrams, new Comparator<Diagram>() {
+
+			public int compare(Diagram d1, Diagram d2) {
+				String d1type = d1.getType();
+				String d2Type = d2.getType();
+				if(d1type.equals(d2Type)) {
+					String string1 = d1.getName();
+					String string2 = d2.getName();
+					return compareNaturalOrder(string1, string2);
+				} else {
+					return d1type.compareTo(d2Type);
+				}
+
+			}
+		});
+		return associatedDiagrams;
 	}
+
+		public static int compareNaturalOrder(String string1, String string2) {
+        	if (string1 == null) {
+        		string1 = "";
+        	}
+        	if (string2 == null) {
+        		string2 = "";
+        	}
+			Matcher m1 = PATTERN_NUMBERS.matcher(string1);
+			Matcher m2 = PATTERN_NUMBERS.matcher(string2);
+			if(!m1.matches() || !m2.matches()) {
+				return string1.compareTo(string2);
+			}
+			if(m1.groupCount() == 1) {
+				return string1.compareTo(string2);
+			} else if(m1.groupCount() == 2) {
+				if(m2.groupCount() == 1) {
+					return string1.compareTo(string2);
+				} else {
+					String group1 = m1.group(1);
+					String group2 = m2.group(1);
+					if(group1.equals(group2)) {
+						try {
+							Integer val1 = Integer.valueOf(m1.group(2));
+							Integer val2 = Integer.valueOf(m2.group(2));
+							return val1.compareTo(val2);
+						} catch (NumberFormatException e) {
+						}
+					} else {
+						return string1.compareTo(string2);
+					}
+				}
+			}
+			return string1.compareTo(string2);
+		}
 }
