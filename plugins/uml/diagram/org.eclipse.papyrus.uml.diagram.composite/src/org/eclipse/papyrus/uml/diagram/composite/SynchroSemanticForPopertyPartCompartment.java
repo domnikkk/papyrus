7f89e0367f060193b0894f8a7e996e9e2e002aa2
ptessier <Patrick.Tessier@cea.fr>
2014-04-28 18:47:37 +0200

    433206: Papyrus shall enable local synchronization between graphical element and element in the model https://bugs.eclipse.org/bugs/show_bug.cgi?id=433206
    Change-Id: Ib5c3e57812f253d29dd9e07c5a6ce204f8166e83

---
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.composite/src/org/eclipse/papyrus/uml/diagram/composite/SynchroSemanticForPopertyPartCompartment.java
@@ -0,0 +1,52 @@
+/*****************************************************************************
+ * Copyright (c) 2014 CEA LIST.
+ *
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.papyrus.uml.diagram.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.papyrus.uml.diagram.synchronizeview.customization.ISynchroSemanticChild;
+import org.eclipse.uml2.uml.Property;
+
+/**
+ *This class is used to precise semantic element that could be displayed in compartment of parts  
+ */
+public class SynchroSemanticForPopertyPartCompartment implements ISynchroSemanticChild {
+
+	public SynchroSemanticForPopertyPartCompartment() {
+	}
+
+	@Override
+	public List<EObject> getSynchroSemanticChild(EObject semanticFromEditPart) {
+		ArrayList<EObject> result=new ArrayList<EObject>();
+		if( semanticFromEditPart instanceof Property){
+			if (((Property)semanticFromEditPart).getType()!=null){
+				result.addAll(((Property)semanticFromEditPart).getType().getOwnedElements());
+			}
+		}
+		return result;
+	}
+	
+	
+	@Override
+	public EObject getSemanticEObjectToListen(EObject semanticFromEditPart) {
+		if( semanticFromEditPart instanceof Property){
+			if (((Property)semanticFromEditPart).getType()!=null){
+				return ((Property)semanticFromEditPart).getType();
+			}
+		}
+		return null;
+	}
+}
