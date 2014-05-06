7850c5925163f371ceb1ad642b878719c31cf588
Camille Letavernier <camille.letavernier@cea.fr>
2014-05-06 10:39:07 +0200

    434176: [Table] Papyrus shall migrate to Nattable 1.1 https://bugs.eclipse.org/bugs/show_bug.cgi?id=434176

--- plugins/infra/nattable/org.eclipse.papyrus.infra.nattable/src/org/eclipse/papyrus/infra/nattable/configuration/StyleConfiguration.java
+++ plugins/infra/nattable/org.eclipse.papyrus.infra.nattable/src/org/eclipse/papyrus/infra/nattable/configuration/StyleConfiguration.java
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Copyright (c) 2012 CEA LIST.
  *
- *    
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,15 +16,16 @@ package org.eclipse.papyrus.infra.nattable.configuration;
 import org.eclipse.nebula.widgets.nattable.config.CellConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.config.DefaultNatTableStyleConfiguration;
 import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
+import org.eclipse.nebula.widgets.nattable.export.ExportConfigAttributes;
 import org.eclipse.papyrus.infra.nattable.converter.GenericDisplayConverter;
 import org.eclipse.papyrus.infra.nattable.formatter.PapyrusExportFormatter;
 import org.eclipse.papyrus.infra.nattable.painter.CustomizedCellPainter;
 
 /**
  * Register a specific Cellpainter
- * 
+ *
  * @author Vincent Lorenzo
- * 
+ *
  */
 public class StyleConfiguration extends DefaultNatTableStyleConfiguration {
 
@@ -33,7 +34,7 @@ public class StyleConfiguration extends DefaultNatTableStyleConfiguration {
 		super.configureRegistry(configRegistry);
 		configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_PAINTER, new CustomizedCellPainter());
 		configRegistry.registerConfigAttribute(CellConfigAttributes.DISPLAY_CONVERTER, new GenericDisplayConverter());
-		configRegistry.registerConfigAttribute(CellConfigAttributes.EXPORT_FORMATTER, new PapyrusExportFormatter());
+		configRegistry.registerConfigAttribute(ExportConfigAttributes.EXPORT_FORMATTER, new PapyrusExportFormatter());
 
 	}
 
