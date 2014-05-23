3c0fe6670a90eba32a96923c05d93c29d654b600
Remi Schnekenburger <remi.schnekenburger@cea.fr>
2014-05-23 16:12:00 +0200

    434966: [Model Explorer] new child menu shall be updated before official release https://bugs.eclipse.org/bugs/show_bug.cgi?id=434966 - Update SysML Menu -- added new sysml child as modela based -- created new element type
    Change-Id: I146d95d84a767c34cfc6a4216ee633966ec9dbfc
    Signed-off-by: Remi Schnekenburger <remi.schnekenburger@cea.fr>

--- plugins/sysml/org.eclipse.papyrus.sysml.service.types/src/org/eclipse/papyrus/sysml/service/types/helper/ConstraintParameterEditHelperAdvice.java
+++ plugins/sysml/org.eclipse.papyrus.sysml.service.types/src/org/eclipse/papyrus/sysml/service/types/helper/ConstraintParameterEditHelperAdvice.java
@@ -10,15 +10,22 @@
  *****************************************************************************/
 package org.eclipse.papyrus.sysml.service.types.helper;
 
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.emf.type.core.commands.ConfigureElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
 import org.eclipse.papyrus.sysml.constraints.ConstraintBlock;
 import org.eclipse.papyrus.sysml.constraints.ConstraintProperty;
+import org.eclipse.papyrus.uml.service.types.utils.NamedElementHelper;
 import org.eclipse.uml2.uml.AggregationKind;
 import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.NamedElement;
 import org.eclipse.uml2.uml.Property;
 import org.eclipse.uml2.uml.Type;
 import org.eclipse.uml2.uml.util.UMLUtil;
@@ -27,6 +34,9 @@ import org.eclipse.uml2.uml.util.UMLUtil;
  *	ConstraintParameter are defined in ConstraintProperty but really owned by ConstraintBlock  
  */
 public class ConstraintParameterEditHelperAdvice extends AbstractEditHelperAdvice {
+
+	/** root name */
+	private static final String PARAMETER = "parameter";
 
 	@Override
 	public ICommand getBeforeEditCommand(IEditCommandRequest request) {
@@ -47,6 +57,25 @@ public class ConstraintParameterEditHelperAdvice extends AbstractEditHelperAdvic
 			}
 		}
 		return super.getBeforeEditCommand(request);
+	}
+	
+	/** Complete creation process by applying the expected stereotype */
+	@Override
+	protected ICommand getBeforeConfigureCommand(final ConfigureRequest request) {
+
+		return new ConfigureElementCommand(request) {
+
+			protected CommandResult doExecuteWithResult(IProgressMonitor progressMonitor, IAdaptable info) throws ExecutionException {
+				NamedElement element = (NamedElement)request.getElementToConfigure();
+				if(element != null) {
+					// Set default name
+					// Initialize the element name based on the created IElementType
+					String initializedName = NamedElementHelper.getDefaultNameWithIncrementFromBase(PARAMETER, element.eContainer().eContents());
+					element.setName(initializedName);
+				}
+				return CommandResult.newOKCommandResult(element);
+			}
+		};
 	}
 	
 }
