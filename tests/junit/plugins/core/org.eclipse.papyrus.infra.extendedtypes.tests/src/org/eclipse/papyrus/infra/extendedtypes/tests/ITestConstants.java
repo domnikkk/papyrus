aed9393d5e86ffc6311a92df4e07daba7db60f13
Remi Schnekenburger <remi.schnekenburger@cea.fr>
2014-06-06 12:13:52 +0200

    - Add missing headers in the test files - add some missing //$NON-NLS-1$ comment for String in Interfaces Change-Id: I8d028d6819f4756232b93e8c683f4512ae5b7666 Signed-off-by: Remi Schnekenburger <remi.schnekenburger@cea.fr>

--- tests/junit/plugins/core/org.eclipse.papyrus.infra.extendedtypes.tests/src/org/eclipse/papyrus/infra/extendedtypes/tests/ITestConstants.java
+++ tests/junit/plugins/core/org.eclipse.papyrus.infra.extendedtypes.tests/src/org/eclipse/papyrus/infra/extendedtypes/tests/ITestConstants.java
@@ -16,25 +16,26 @@ package org.eclipse.papyrus.infra.extendedtypes.tests;
  * Constants for test classes
  */
 public interface ITestConstants {
-	public static final String ORG_ECLIPSE_PAPYRUS_UML_CLASS = "org.eclipse.papyrus.uml.Class";
-	public final static String ABSTRACT_CLASS_TOOL = "org.eclipse.papyrus.infra.extendedtypes.tests.aspectabstractclass"; //$NON-NLS-1$
-	/** name of the extended type set in Test1 */
-	public final static String ExtendedTypeSetName_Test1 = "org.eclipse.papyrus.infra.extendedtypes.tests.tests.test1";
-	
-	public final static String WORKSPACE_ELEMENT_TYPE_ID = "org.eclipse.papyrus.infra.extendedtypes.tests.workspacetype"; //$NON-NLS-1$
-	
-	public final static String WORKSPACE_ELEMENT_TYPE_TOOL = "org.eclipse.papyrus.infra.extendedtypes.tests.tool"; //$NON-NLS-1$
-	
-	static final String PACKAGE_WITH_COMPONENTS = "PackageWithComponents";
+	static final String ORG_ECLIPSE_PAPYRUS_UML_CLASS = "org.eclipse.papyrus.uml.Class"; //$NON-NLS-1$
 
-	static final String PACKAGE_WITH_ALL_ELEMENTS = "PackageWithAllElements";
-	
-	static final String OTHER_PACKAGE_WITH_COMPONENTS = "OtherPackageWithComponents";
-	
-	static final String PACKAGE_WITH_COMPONENTS_TYPE_ID = "org.eclipse.papyrus.infra.extendedtypes.tests.packagewithcomponentsonly";
-	
-	static final String SPECIFICCOMPONENT_TYPE_ID = "org.eclipse.papyrus.infra.extendedtypes.tests.specificcomponent";
+	final static String ABSTRACT_CLASS_TOOL = "org.eclipse.papyrus.infra.extendedtypes.tests.aspectabstractclass"; //$NON-NLS-1$
 
-	static final String COMPONENT_NAME = "Component1";
+	final static String ExtendedTypeSetName_Test1 = "org.eclipse.papyrus.infra.extendedtypes.tests.tests.test1"; //$NON-NLS-1$
+
+	final static String WORKSPACE_ELEMENT_TYPE_ID = "org.eclipse.papyrus.infra.extendedtypes.tests.workspacetype"; //$NON-NLS-1$
+
+	final static String WORKSPACE_ELEMENT_TYPE_TOOL = "org.eclipse.papyrus.infra.extendedtypes.tests.tool"; //$NON-NLS-1$
+
+	static final String PACKAGE_WITH_COMPONENTS = "PackageWithComponents"; //$NON-NLS-1$
+
+	static final String PACKAGE_WITH_ALL_ELEMENTS = "PackageWithAllElements"; //$NON-NLS-1$
+
+	static final String OTHER_PACKAGE_WITH_COMPONENTS = "OtherPackageWithComponents"; //$NON-NLS-1$
+
+	static final String PACKAGE_WITH_COMPONENTS_TYPE_ID = "org.eclipse.papyrus.infra.extendedtypes.tests.packagewithcomponentsonly"; //$NON-NLS-1$
+
+	static final String SPECIFICCOMPONENT_TYPE_ID = "org.eclipse.papyrus.infra.extendedtypes.tests.specificcomponent"; //$NON-NLS-1$
+
+	static final String COMPONENT_NAME = "Component1"; //$NON-NLS-1$
 
 }
