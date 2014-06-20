4160479cc8a93a9e3f3fd564c75dbd08f60b1637
Camille Letavernier <camille.letavernier@cea.fr>
2014-06-20 19:33:08 +0200

    437807: [Moka - Releng] Moka shall be integrated as an extra-plugin in Papyrus https://bugs.eclipse.org/bugs/show_bug.cgi?id=437807
    - Fix Bundle Metadata

--- extraplugins/moka/org.eclipse.papyrus.moka.fuml/src/org/eclipse/papyrus/moka/fuml/Activator.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml/src/org/eclipse/papyrus/moka/fuml/Activator.java
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Copyright (c) 2012 CEA LIST.
  *
- *    
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,7 +24,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	/** The Constant PLUGIN_ID. */
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.fuml"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.moka.fuml"; //$NON-NLS-1$
 
 	// The shared instance
 	/** The plugin. */
@@ -38,11 +38,12 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
 	 * )
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -50,11 +51,12 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
 	 * )
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
--- tests/junit/extraplugins/moka/org.eclipse.papyrus.moka.tests/src/org/eclipse/papyrus/moka/tests/Activator.java
+++ tests/junit/extraplugins/moka/org.eclipse.papyrus.moka.tests/src/org/eclipse/papyrus/moka/tests/Activator.java
@@ -22,7 +22,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.moka.composites.tests"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.moka.tests"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
@@ -37,9 +37,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -48,9 +49,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
