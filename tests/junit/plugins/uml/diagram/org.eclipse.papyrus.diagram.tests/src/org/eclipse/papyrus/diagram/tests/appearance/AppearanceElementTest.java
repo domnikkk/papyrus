a4420c351de7d7565218f6975b19eaac90fb2075
ptessier <Patrick.Tessier@cea.fr>
2014-07-02 17:49:12 +0200

    399876: [Test] Improve Papyrus test coverage https://bugs.eclipse.org/bugs/show_bug.cgi?id=399876
    Change-Id: I790dd954703795d8623ca492ed2bcb88ec5fd41c

--- tests/junit/plugins/uml/diagram/org.eclipse.papyrus.diagram.tests/src/org/eclipse/papyrus/diagram/tests/appearance/AppearanceElementTest.java
+++ tests/junit/plugins/uml/diagram/org.eclipse.papyrus.diagram.tests/src/org/eclipse/papyrus/diagram/tests/appearance/AppearanceElementTest.java
@@ -12,6 +12,41 @@
  /*****************************************************************************/
 package org.eclipse.papyrus.diagram.tests.appearance;
 
+import org.eclipse.emf.common.command.CommandStack;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.papyrus.commands.wrappers.GEFtoEMFCommandWrapper;
+import org.eclipse.papyrus.diagram.tests.canonical.AbstractPapyrusTestCase;
+import org.eclipse.swt.widgets.Display;
 
-public class AppearanceElementTest {
+
+public abstract class AppearanceElementTest {
+	
+	/**
+	 * Call {@link AbstractPapyrusTestCase#execute(Command) execute} on the UI
+	 * thread.
+	 */
+	protected void executeOnUIThread(final Command command) {
+		Display.getDefault().syncExec(new Runnable() {
+
+			public void run() {
+				execute(command);
+			}
+		});
+	}
+	
+	
+	
+	/** Execute the given command in the diagram editor. */
+	protected void execute(final Command command) {
+		getCommandStack().execute(new GEFtoEMFCommandWrapper(command));
+	}
+
+
+
+	abstract CommandStack getCommandStack(); 
+	abstract void testAppearance(IElementType type);
+	abstract GraphicalEditPart getContainerEditPart();
+
 }
--- tests/junit/plugins/uml/diagram/org.eclipse.papyrus.diagram.tests/src/org/eclipse/papyrus/diagram/tests/appearance/AppearanceNodeTest.java
+++ tests/junit/plugins/uml/diagram/org.eclipse.papyrus.diagram.tests/src/org/eclipse/papyrus/diagram/tests/appearance/AppearanceNodeTest.java
@@ -12,6 +12,74 @@
  /*****************************************************************************/
 package org.eclipse.papyrus.diagram.tests.appearance;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-public class AppearanceNodeTest  extends AppearanceElementTest{
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest;
+import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequestFactory;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.swt.widgets.Display;
+import org.junit.Assert;
+
+/**
+ * This abstract class is used to test the appearance of a node
+ *
+ */
+public abstract class AppearanceNodeTest  extends AppearanceElementTest{
+	protected Command command = null;
+
+	/**
+	 * Test to create a node.
+	 *
+	 * @param type
+	 *        the type
+	 */
+	public GraphicalEditPart createANode(IElementType type, final GraphicalEditPart containerEditPart) {
+
+		//CREATION
+		final CreateViewRequest requestcreation = CreateViewRequestFactory.getCreateShapeRequest(type, containerEditPart.getDiagramPreferencesHint());
+		Display.getDefault().syncExec(new Runnable() {
+
+			public void run() {
+				command = containerEditPart.getCommand(requestcreation);
+			}
+		});
+		assertNotNull("the command must not be null", command);
+		assertTrue("the command must be executable", command.canExecute()); //$NON-NLS-1$
+		// execute the creation
+		executeOnUIThread(command);
+
+
+		GraphicalEditPart createdEditPart= (GraphicalEditPart)containerEditPart.getChildren().get((containerEditPart.getChildren().size()-1));
+		Assert.assertNotNull("The editpart must be created", createdEditPart); //$NON-NLS-1$
+		return  createdEditPart;
+
+	}
+	 public abstract void testBackgroundColor(GraphicalEditPart createdEditpart);
+	 public abstract void testForegroundColor(GraphicalEditPart createdEditpart);
+	 public abstract void testGradient(GraphicalEditPart createdEditpart);
+	 public abstract void testLineWidth(GraphicalEditPart createdEditpart);
+	 public abstract void testTransparency(GraphicalEditPart createdEditpart);
+	 public abstract void testFont(GraphicalEditPart createdEditpart);
+	 public abstract void testFontHeight(GraphicalEditPart createdEditpart);
+	 public abstract void testElementIcon(GraphicalEditPart createdEditpart);
+	 public abstract void testShadow(GraphicalEditPart createdEditpart);
+	
+	@Override
+	void testAppearance(IElementType type) {
+		GraphicalEditPart createdEditpart=createANode(type, getContainerEditPart());
+		testBackgroundColor(createdEditpart);
+		testForegroundColor(createdEditpart);
+		testGradient(createdEditpart);
+		testLineWidth(createdEditpart);
+		testTransparency(createdEditpart);
+		testFont(createdEditpart);
+		testFontHeight(createdEditpart);
+		testElementIcon(createdEditpart);
+		testShadow(createdEditpart);
+		
+		
+	}
 }
