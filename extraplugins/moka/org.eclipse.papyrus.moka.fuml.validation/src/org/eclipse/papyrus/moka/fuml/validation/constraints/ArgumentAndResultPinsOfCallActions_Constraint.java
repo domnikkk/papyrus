c547a566a37d193bdcedd9367f97f8fa6583d27e
Camille Letavernier <camille.letavernier@cea.fr>
2014-08-01 16:29:39 +0200

    439644: [Moka] [Validation] Moka shall contribute some validation rules https://bugs.eclipse.org/bugs/show_bug.cgi?id=439644
    - Fix build path (Java 6)

--- extraplugins/moka/org.eclipse.papyrus.moka.fuml.validation/src/org/eclipse/papyrus/moka/fuml/validation/constraints/ArgumentAndResultPinsOfCallActions_Constraint.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml.validation/src/org/eclipse/papyrus/moka/fuml/validation/constraints/ArgumentAndResultPinsOfCallActions_Constraint.java
@@ -31,77 +31,73 @@ import org.eclipse.uml2.uml.Signal;
 /**
  * This class is introduced to check the following constraint:
  * Argument / result pins of call actions shall match parameters of invoked / sent Operation / Behavior / Signal
- * See 439648: [Moka] [Validation]  Argument / result pins of call actions shall match parameters of invoked / sent Operation / Behavior / Signal https://bugs.eclipse.org/bugs/show_bug.cgi?id=439648
- * 
+ * See 439648: [Moka] [Validation] Argument / result pins of call actions shall match parameters of invoked / sent Operation / Behavior / Signal
+ * https://bugs.eclipse.org/bugs/show_bug.cgi?id=439648
+ *
  */
 public class ArgumentAndResultPinsOfCallActions_Constraint extends AbstractModelConstraint {
 
 	@Override
 	public IStatus validate(IValidationContext ctx) {
 		// fUML / PSCS support three kinds of call actions: CallOperationAction, CallBehaviorAction, SendSignalAction
-		// First gather relevant information to validate argument / result pins of the call, 
+		// First gather relevant information to validate argument / result pins of the call,
 		// which a an approach specific to each kind of action.
 		// From the standpoint of the fUML Execution Engine, only the number of pins are relevant
-		List<ConnectableElement> inputParameters = new ArrayList<ConnectableElement>() ;
-		List<ConnectableElement> outputParameters = new ArrayList<ConnectableElement>() ;
-		if (ctx.getTarget() instanceof CallOperationAction || ctx.getTarget() instanceof CallBehaviorAction) {
-			List<Parameter> allParameters = new ArrayList<>() ;
-			if (ctx.getTarget() instanceof CallOperationAction) {
-				Operation op = ((CallOperationAction)ctx.getTarget()).getOperation() ;
-				if (op != null) {
-					allParameters.addAll(op.getOwnedParameters()) ;
+		List<ConnectableElement> inputParameters = new ArrayList<ConnectableElement>();
+		List<ConnectableElement> outputParameters = new ArrayList<ConnectableElement>();
+		if(ctx.getTarget() instanceof CallOperationAction || ctx.getTarget() instanceof CallBehaviorAction) {
+			List<Parameter> allParameters = new ArrayList<Parameter>();
+			if(ctx.getTarget() instanceof CallOperationAction) {
+				Operation op = ((CallOperationAction)ctx.getTarget()).getOperation();
+				if(op != null) {
+					allParameters.addAll(op.getOwnedParameters());
+				}
+			} else if(ctx.getTarget() instanceof CallBehaviorAction) {
+				Behavior behav = ((CallBehaviorAction)ctx.getTarget()).getBehavior();
+				if(behav != null) {
+					allParameters.addAll(behav.getOwnedParameters());
 				}
 			}
-			else if (ctx.getTarget() instanceof CallBehaviorAction) {
-				Behavior behav = ((CallBehaviorAction)ctx.getTarget()).getBehavior() ;
-				if (behav != null) {
-					allParameters.addAll(behav.getOwnedParameters()) ;
+			for(Parameter p : allParameters) {
+				if(p.getDirection().equals(ParameterDirectionKind.IN_LITERAL) || p.getDirection().equals(ParameterDirectionKind.INOUT_LITERAL)) {
+					inputParameters.add(p);
+				}
+				if(p.getDirection().equals(ParameterDirectionKind.INOUT_LITERAL) || p.getDirection().equals(ParameterDirectionKind.OUT_LITERAL) || p.getDirection().equals(ParameterDirectionKind.RETURN_LITERAL)) {
+					outputParameters.add(p);
 				}
 			}
-			for (Parameter p : allParameters) {
-				if (p.getDirection().equals(ParameterDirectionKind.IN_LITERAL) || p.getDirection().equals(ParameterDirectionKind.INOUT_LITERAL)) {
-					inputParameters.add(p) ;
-				}
-				if (p.getDirection().equals(ParameterDirectionKind.INOUT_LITERAL) || p.getDirection().equals(ParameterDirectionKind.OUT_LITERAL) 
-						|| p.getDirection().equals(ParameterDirectionKind.RETURN_LITERAL)) {
-					outputParameters.add(p) ;
-				}
-			}
-		}
-		else if (ctx.getTarget() instanceof SendSignalAction) {
-			Signal s = ((SendSignalAction)ctx.getTarget()).getSignal() ;
-			if (s != null) {
-				inputParameters.addAll(s.getAllAttributes()) ;
+		} else if(ctx.getTarget() instanceof SendSignalAction) {
+			Signal s = ((SendSignalAction)ctx.getTarget()).getSignal();
+			if(s != null) {
+				inputParameters.addAll(s.getAllAttributes());
 			}
 		}
 
-		CallAction callAction = (CallAction)ctx.getTarget() ;
-		String argumentsErrorMessage = "" ;
-		if (callAction.getArguments().size() < inputParameters.size()) {
-			argumentsErrorMessage += "Not enough argument pins (found " + callAction.getArguments().size() + ", expected " + inputParameters.size() + ")" ;
+		CallAction callAction = (CallAction)ctx.getTarget();
+		String argumentsErrorMessage = "";
+		if(callAction.getArguments().size() < inputParameters.size()) {
+			argumentsErrorMessage += "Not enough argument pins (found " + callAction.getArguments().size() + ", expected " + inputParameters.size() + ")";
+		} else if(callAction.getArguments().size() > inputParameters.size()) {
+			argumentsErrorMessage += "Too many argument pins (found " + callAction.getArguments().size() + ", expected " + inputParameters.size() + ")";
 		}
-		else if (callAction.getArguments().size() > inputParameters.size()) {
-			argumentsErrorMessage += "Too many argument pins (found " + callAction.getArguments().size() + ", expected " + inputParameters.size() + ")" ;
-		}
-		String resultsErrorMessage = "" ;
-		if (callAction.getResults().size() < outputParameters.size()) {
-			resultsErrorMessage += "Not enough result pins (found " + callAction.getResults().size() + ", expected " + outputParameters.size() + ")" ;
-		}
-		else if (callAction.getResults().size() > outputParameters.size()) {
-			if (callAction.getResults().size() < outputParameters.size()) {
-				resultsErrorMessage += "Too many result pins (found " + callAction.getResults().size() + ", expected " + outputParameters.size() + ")" ;
+		String resultsErrorMessage = "";
+		if(callAction.getResults().size() < outputParameters.size()) {
+			resultsErrorMessage += "Not enough result pins (found " + callAction.getResults().size() + ", expected " + outputParameters.size() + ")";
+		} else if(callAction.getResults().size() > outputParameters.size()) {
+			if(callAction.getResults().size() < outputParameters.size()) {
+				resultsErrorMessage += "Too many result pins (found " + callAction.getResults().size() + ", expected " + outputParameters.size() + ")";
 			}
 		}
 
-		if (argumentsErrorMessage.equals("") && resultsErrorMessage.equals("")) {
-			return ctx.createSuccessStatus() ;
+		if(argumentsErrorMessage.equals("") && resultsErrorMessage.equals("")) {
+			return ctx.createSuccessStatus();
 		}
 
-		if (! argumentsErrorMessage.equals("")) {
-			resultsErrorMessage = " - " + resultsErrorMessage ;
+		if(!argumentsErrorMessage.equals("")) {
+			resultsErrorMessage = " - " + resultsErrorMessage;
 		}
 
-		return ctx.createFailureStatus(argumentsErrorMessage + resultsErrorMessage) ;
+		return ctx.createFailureStatus(argumentsErrorMessage + resultsErrorMessage);
 	}
 
 }
