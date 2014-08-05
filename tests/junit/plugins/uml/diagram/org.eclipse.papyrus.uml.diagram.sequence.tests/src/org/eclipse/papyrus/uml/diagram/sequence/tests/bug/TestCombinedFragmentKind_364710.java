d4f84988b194f95a55d0e4e4578bfbcbce31b2e1
sboyko <serg.boyko2011@gmail.com>
2014-08-05 18:27:56 +0400

    [437605] - Export Sequence Diagram to Images
    Change-Id: I70b6a5c7efa036fd023c90ee15e63fb98703bbfe
    Signed-off-by: sboyko <serg.boyko2011@gmail.com>

--- tests/junit/plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence.tests/src/org/eclipse/papyrus/uml/diagram/sequence/tests/bug/TestCombinedFragmentKind_364710.java
+++ tests/junit/plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence.tests/src/org/eclipse/papyrus/uml/diagram/sequence/tests/bug/TestCombinedFragmentKind_364710.java
@@ -16,6 +16,9 @@ package org.eclipse.papyrus.uml.diagram.sequence.tests.bug;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.emf.ecore.EAttribute;
@@ -23,6 +26,8 @@ import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.UnexecutableCommand;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.diagram.ui.image.ImageFileFormat;
+import org.eclipse.gmf.runtime.diagram.ui.render.util.CopyToImageUtil;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequestFactory;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
@@ -34,11 +39,14 @@ import org.eclipse.papyrus.infra.services.edit.service.IElementEditService;
 import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CombinedFragmentCombinedFragmentCompartmentEditPart;
 import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CombinedFragmentEditPart;
 import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CustomConsiderIgnoreFragmentEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CustomContinuationEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.InteractionOperandEditPart;
 import org.eclipse.papyrus.uml.diagram.sequence.providers.UMLElementTypes;
 import org.eclipse.papyrus.uml.diagram.sequence.tests.ISequenceDiagramTestsConstants;
 import org.eclipse.papyrus.uml.diagram.sequence.tests.canonical.CreateSequenceDiagramCommand;
 import org.eclipse.papyrus.uml.diagram.sequence.tests.canonical.TestTopNode;
 import org.eclipse.uml2.uml.CombinedFragment;
+import org.eclipse.uml2.uml.Continuation;
 import org.eclipse.uml2.uml.InteractionOperatorKind;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.junit.Test;
@@ -56,6 +64,10 @@ import org.junit.Test;
 public class TestCombinedFragmentKind_364710 extends TestTopNode {
 
 	private static final String CHANGE_OPERATOR_KIND = "Change Operator Kind: ";
+
+	private static final String CHANGE_CONTINUATION_SETTING = "Change Continuation Setting: ";
+
+	private static final String COPY_TO_IMAGE = "Copy To Image: ";
 
 	@Override
 	protected ICreationCommand getDiagramCommandCreation() {
@@ -107,6 +119,30 @@ public class TestCombinedFragmentKind_364710 extends TestTopNode {
 		}
 	}
 
+	@Test
+	public void testCombinedFragmentContinuation() throws CoreException {
+		createNode(UMLElementTypes.CombinedFragment_3004, getRootEditPart(), new Point(10, 80), new Dimension(100, 100));
+		CombinedFragmentEditPart cep = (CombinedFragmentEditPart)getRootEditPart().getChildren().get(0);
+		final CombinedFragmentCombinedFragmentCompartmentEditPart cfp = (CombinedFragmentCombinedFragmentCompartmentEditPart)cep.getChildren().get(0);
+
+		CombinedFragment cf = (CombinedFragment)cep.resolveSemanticElement();
+		changeOperatorKind(cep, cf, InteractionOperatorKind.ALT_LITERAL);
+		
+		// add continuation
+		InteractionOperandEditPart op = (InteractionOperandEditPart)cfp.getChildren().get(0);
+		createNode(UMLElementTypes.Continuation_3016, op, new Point(1, 1), new Dimension(20, 60));
+		assertTrue(CREATION + TEST_THE_EXECUTION, op.getChildren().size() == 2);
+		CustomContinuationEditPart ccep = (CustomContinuationEditPart)op.getChildren().get(1);
+		Continuation continuation = (Continuation) ccep.resolveSemanticElement();
+		changeContinuationSetting(ccep, continuation, false);
+
+		// export to image
+		CopyToImageUtil copyToImageUtil = new CopyToImageUtil();
+		IPath path = project.getFile(getFileName() + '.' + ImageFileFormat.PNG.getName()).getRawLocation();
+		copyToImageUtil.copyToImage(getDiagramEditPart(), path, ImageFileFormat.PNG, new NullProgressMonitor());
+		assertTrue(COPY_TO_IMAGE + TEST_THE_EXECUTION, path.toFile().exists());
+	}
+
 	protected void changeOperatorKind(CombinedFragmentEditPart p, CombinedFragment cf, InteractionOperatorKind kind) {
 		EAttribute feature = UMLPackage.eINSTANCE.getCombinedFragment_InteractionOperator();
 		IElementEditService provider = ElementEditServiceUtils.getCommandProvider(cf);
@@ -117,6 +153,18 @@ public class TestCombinedFragmentKind_364710 extends TestTopNode {
 		getEMFCommandStack().execute(emfCommand);
 		waitForComplete();
 		assertTrue(CHANGE_OPERATOR_KIND + TEST_THE_EXECUTION, cf.getInteractionOperator() == kind);
+	}
+
+	protected void changeContinuationSetting(CustomContinuationEditPart p, Continuation cf, Boolean isSetting) {
+		EAttribute feature = UMLPackage.eINSTANCE.getContinuation_Setting();
+		IElementEditService provider = ElementEditServiceUtils.getCommandProvider(cf);
+		SetRequest request = new SetRequest(p.getEditingDomain(), cf, feature, isSetting);
+		ICommand createGMFCommand = provider.getEditCommand(request);
+		org.eclipse.emf.common.command.AbstractCommand emfCommand = new GMFtoEMFCommandWrapper(createGMFCommand);
+		assertTrue(CHANGE_CONTINUATION_SETTING + TEST_IF_THE_COMMAND_CAN_BE_EXECUTED, emfCommand.canExecute() == true);
+		getEMFCommandStack().execute(emfCommand);
+		waitForComplete();
+		assertTrue(CHANGE_CONTINUATION_SETTING + TEST_THE_EXECUTION, cf.isSetting() == isSetting);
 	}
 
 	public void createNode(IElementType type, EditPart parentPart, Point location, Dimension size) {
