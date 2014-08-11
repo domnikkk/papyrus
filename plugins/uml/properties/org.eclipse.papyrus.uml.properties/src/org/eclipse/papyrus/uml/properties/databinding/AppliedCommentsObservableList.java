42be5042ce6b1baba655c5668ec778b5a5dcf7cf
Gabriel Pascual <gabriel.pascual@all4tec.net>
2014-08-11 17:58:50 +0200

    441511: [Properties View] Deletion of several applied comments https://bugs.eclipse.org/bugs/show_bug.cgi?id=441511
     - Fixes add and remove behaviour
    
    Change-Id: I2db04d86ada363d611c9ea4a56278a5e8d675edd
    Signed-off-by: Gabriel Pascual <gabriel.pascual@all4tec.net>

--- plugins/uml/properties/org.eclipse.papyrus.uml.properties/src/org/eclipse/papyrus/uml/properties/databinding/AppliedCommentsObservableList.java
+++ plugins/uml/properties/org.eclipse.papyrus.uml.properties/src/org/eclipse/papyrus/uml/properties/databinding/AppliedCommentsObservableList.java
@@ -9,10 +9,10 @@
  * Contributors:
  *  Sebastien Poissonnet (CEA LIST) sebastien.poissonnet@cea.fr
  *  Mickaël ADAM (ALL4TEC) mickael.adam@all4tec.net - bug 435174
+ *  Gabriel Pascual (ALL4TEC)  -  Bug 441511
  *****************************************************************************/
 package org.eclipse.papyrus.uml.properties.databinding;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
@@ -23,27 +23,34 @@ import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature.Setting;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
-import org.eclipse.papyrus.infra.services.edit.service.IElementEditService;
 import org.eclipse.papyrus.uml.tools.databinding.PapyrusObservableList;
 import org.eclipse.uml2.common.util.UML2Util;
 import org.eclipse.uml2.uml.Comment;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.UMLPackage;
 
+/**
+ * Observable list for applied comments.
+ */
 public class AppliedCommentsObservableList extends PapyrusObservableList {
 
 	public AppliedCommentsObservableList(EditingDomain domain, Element source) {
 		super(getAppliedCommentsList(source), domain, source, UMLPackage.eINSTANCE.getElement_OwnedComment());
 	}
 
+	/**
+	 * Gets the applied comments list.
+	 *
+	 * @param source
+	 *        the source
+	 * @return the applied comments list
+	 */
 	private static List<Comment> getAppliedCommentsList(Element source) {
 		List<Comment> result = new LinkedList<Comment>();
 		Iterator<Setting> it = UML2Util.getNonNavigableInverseReferences(source).iterator();
@@ -75,6 +82,14 @@ public class AppliedCommentsObservableList extends PapyrusObservableList {
 		return result;
 	}
 
+	/**
+	 * <p>
+	 * Redefine refresh cache because applied comments list is a subset of {@link Element#getOwnedComments <em>Owned Comments</em>}.
+	 * </p>
+	 * 
+	 * @see org.eclipse.papyrus.infra.emf.databinding.EMFObservableList#refreshCacheList()
+	 *
+	 */
 	@Override
 	protected void refreshCacheList() {
 		if(isDisposed()) {
@@ -85,121 +100,86 @@ public class AppliedCommentsObservableList extends PapyrusObservableList {
 		fireListChange(null);
 	}
 
-	@Override
-	public Command getAddAllCommand(Collection<?> values) {
-		throw new UnsupportedOperationException();
-		//		return super.getAddAllCommand(values);
-	}
 
-	@Override
-	public Command getAddAllCommand(int index, Collection<?> values) {
-		throw new UnsupportedOperationException();
-		//		return super.getAddAllCommand(index, values);
-	}
 
-	@Override
-	public Command getAddCommand(int index, Object value) {
-		throw new UnsupportedOperationException();
-		//		return super.getAddCommand(index, value);
-	}
-
+	/**
+	 * @see org.eclipse.papyrus.uml.tools.databinding.PapyrusObservableList#getAddCommand(java.lang.Object)
+	 *
+	 * @param value
+	 * @return
+	 */
 	@Override
 	public Command getAddCommand(Object value) {
-		Assert.isTrue(value instanceof Comment);
-		Comment comment = (Comment)value;
-		//Add the comment to source#ownedComment
-		CompoundCommand addAppliedCommentCommand = new CompoundCommand("Add applied comment");
-		addAppliedCommentCommand.append(super.getAddCommand(value));
-		//Add the source element to comment#annotatedElement
-		//		List<Element> values = new LinkedList<Element>(comment.getAnnotatedElements());
-		//		values.add((Element)source);
-		//		
-		//		SetRequest setRequest = new SetRequest(comment, UMLPackage.eINSTANCE.getComment_AnnotatedElement(), values);
-		//		IElementEditService provider = getProvider();
-		//
-		//		addAppliedCommentCommand.append(getCommandFromRequests(provider, Collections.singletonList(setRequest)));
-		AddCommand addCommand = new AddCommand(editingDomain, comment, UMLPackage.eINSTANCE.getComment_AnnotatedElement(), source);
-		addAppliedCommentCommand.append(addCommand);
+		CompoundCommand addAppliedCommentCommand = null;
+
+		if(value instanceof Comment) {
+
+			addAppliedCommentCommand = new CompoundCommand("Add applied comment");
+
+			//Add the comment to source#ownedComment
+			SetRequest setRequest = new SetRequest(source, feature, value);
+			addAppliedCommentCommand.append(getCommandFromRequests(getProvider(), Collections.singletonList(setRequest)));
+
+			//Check if source was already had to comment 
+			if(!((Comment)value).getAnnotatedElements().contains(source)) {
+				// Add comment to element
+				AddCommand addCommand = new AddCommand(editingDomain, (EObject)value, UMLPackage.eINSTANCE.getComment_AnnotatedElement(), source);
+				addAppliedCommentCommand.append(addCommand);
+			}
+		}
+
 		return addAppliedCommentCommand;
 	}
 
+	/**
+	 * @see org.eclipse.papyrus.uml.tools.databinding.PapyrusObservableList#getRemoveCommand(java.lang.Object)
+	 *
+	 * @param value
+	 * @return
+	 */
 	@Override
 	public Command getRemoveCommand(Object value) {
-		Assert.isTrue(value instanceof Comment);
-		CompoundCommand removeAppliedCommentCommand = new CompoundCommand("Remove applied comment");
-		Comment comment = (Comment)value;
-		if(comment.getAnnotatedElements().size() <= 1) {
-			return super.getRemoveCommand(value);
+
+		Command removeAppliedCommentCommand = null;
+		if(value instanceof Comment) {
+
+			Comment comment = (Comment)value;
+
+			if(comment.getAnnotatedElements().size() > 1) {
+				//Remove on link between source and comment
+				List<Element> values = new LinkedList<Element>(comment.getAnnotatedElements());
+				values.remove(source);
+				SetRequest setRequest = new SetRequest(comment, UMLPackage.eINSTANCE.getComment_AnnotatedElement(), values);
+				removeAppliedCommentCommand = getCommandFromRequests(getProvider(), Collections.singletonList(setRequest));
+
+			} else {
+				//Remove comment in element
+				DestroyElementRequest detroyRequest = new DestroyElementRequest((TransactionalEditingDomain)editingDomain, comment, false);
+				removeAppliedCommentCommand = getCommandFromRequests(getProvider(), Collections.singleton(detroyRequest));
+			}
+
 		}
-		//Remove the source element to comment#annotatedElement
-		List<Element> values = new LinkedList<Element>(comment.getAnnotatedElements());
-		values.remove(source);
-		SetRequest setRequest = new SetRequest(comment, UMLPackage.eINSTANCE.getComment_AnnotatedElement(), values);
-		IElementEditService provider = getProvider();
-		removeAppliedCommentCommand.append(getCommandFromRequests(provider, Collections.singletonList(setRequest)));
-		//		if(editingDomain != null) {
-		//			RemoveCommand command = new RemoveCommand(editingDomain, comment, UMLPackage.eINSTANCE.getComment_AnnotatedElement(), source);
-		//			return command;
-		//		}
+
 		return removeAppliedCommentCommand;
 	}
 
+	/**
+	 * @see org.eclipse.papyrus.uml.tools.databinding.PapyrusObservableList#getRemoveAllCommand(java.util.Collection)
+	 *
+	 * @param values
+	 * @return
+	 */
 	@Override
 	public Command getRemoveAllCommand(Collection<?> values) {
 		Iterator<?> itr = values.iterator();
-		Comment comment;
 		Element value;
-		List<Element> values_;
 		CompoundCommand removeAppliedCommentCommand = new CompoundCommand("Remove applied comment");
 		while(itr.hasNext()) {
 			value = (Element)itr.next();
 			Assert.isTrue(value instanceof Comment);
-			comment = (Comment)value;
-			if(comment.getAnnotatedElements().size() <= 1) {
-				removeAppliedCommentCommand.append(super.getRemoveCommand(value));
-			} else {
-				values_ = new LinkedList<Element>(comment.getAnnotatedElements());
-				values_.remove(source);
-				SetRequest setRequest = new SetRequest(comment, UMLPackage.eINSTANCE.getComment_AnnotatedElement(), values_);
-				IElementEditService provider = getProvider();
-				removeAppliedCommentCommand.append(getCommandFromRequests(provider, Collections.singletonList(setRequest)));
-			}
+			removeAppliedCommentCommand.append(getRemoveCommand(value));
 		}
 		return removeAppliedCommentCommand;
-	}
-
-	//override to patch bug 435174
-	@Override
-	protected Collection<? extends IEditCommandRequest> getRequests(List<Object> newValues, Collection<?> removedValues) {
-		LinkedList<IEditCommandRequest> requests = new LinkedList<IEditCommandRequest>();
-		//DestroyElement request
-		if(feature instanceof EReference && ((EReference)feature).isContainment() && removedValues != null) {
-			for(Object o : removedValues) {
-				if(o instanceof EObject) {
-					requests.add(new DestroyElementRequest((TransactionalEditingDomain)editingDomain, (EObject)o, false));
-				}
-			}
-		}
-		//List to take into account the no annotated owned comment
-		List<Object> ownedComments = new ArrayList<Object>();
-		//Look for all owned comments of the source
-		for(EObject ownedComment : ((Element)source).getOwnedComments()) {
-			//Add comment the list
-			if(removedValues == null || !removedValues.contains(ownedComment)) {
-				ownedComments.add(ownedComment);
-			}
-		}
-		//Add new values
-		for(Object value : newValues) {
-			if(value instanceof Comment) {
-				//Only if the comment isn't owned by an other object
-				if(((Comment)value).eContainer() == null) {
-					ownedComments.add(value);
-				}
-			}
-		}
-		requests.add(new SetRequest((TransactionalEditingDomain)editingDomain, source, feature, ownedComments));
-		return requests;
 	}
 
 	//
@@ -227,5 +207,23 @@ public class AppliedCommentsObservableList extends PapyrusObservableList {
 	public Command getSetCommand(int index, Object value) {
 		throw new UnsupportedOperationException();
 		//		return super.getSetCommand(index, value);
+	}
+
+	@Override
+	public Command getAddAllCommand(Collection<?> values) {
+		throw new UnsupportedOperationException();
+		//		return super.getAddAllCommand(values);
+	}
+
+	@Override
+	public Command getAddAllCommand(int index, Collection<?> values) {
+		throw new UnsupportedOperationException();
+		//		return super.getAddAllCommand(index, values);
+	}
+
+	@Override
+	public Command getAddCommand(int index, Object value) {
+		throw new UnsupportedOperationException();
+		//		return super.getAddCommand(index, value);
 	}
 }
