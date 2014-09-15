f6f7e9dc6a6d1c91b94197c176cf9a54a0b60b38
Camille Letavernier <camille.letavernier@cea.fr>
2014-09-15 17:52:13 +0200

    [Releng] Update the expected version number in Bundle Tests

--- tests/junit/plugins/developer/org.eclipse.papyrus.bundles.tests/src/org/eclipse/papyrus/bundles/tests/BundleTestsUtils.java
+++ tests/junit/plugins/developer/org.eclipse.papyrus.bundles.tests/src/org/eclipse/papyrus/bundles/tests/BundleTestsUtils.java
@@ -16,7 +16,7 @@ import org.osgi.framework.BundleContext;
 public class BundleTestsUtils {
 
 	// This version number should be changed at each release
-	public static final String PAPYRUS_VERSION = "1.0.1"; //$NON-NLS-1$
+	public static final String PAPYRUS_VERSION = "1.1.0"; //$NON-NLS-1$
 
 	public static final String INCUBATION = "(Incubation)"; //$NON-NLS-1$
 
