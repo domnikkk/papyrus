675a4817c31596849911adb46edff2eb4612fa9a
Gabriel Pascual <gabriel.pascual@all4tec.net>
2014-09-18 09:09:30 +0200

    436952: [Submodel] Deletion of a model fragment does not delete the model fragment resources https://bugs.eclipse.org/bugs/show_bug.cgi?id=436952
    -  Shift the handlers and contributions of Model Explorer menu to Model
    Explorer"plugin :  "Create submodel" and "Reintegrate submodel"
      - Add Uncontrol of control mode to delete actions ( Model explorer and
    Diagram)
    
    Change-Id: I601cc4cd482e2b2d2f5a6acfc0b3bce76fa69b82
    Signed-off-by: Gabriel Pascual <gabriel.pascual@all4tec.net>

--- plugins/views/modelexplorer/org.eclipse.papyrus.views.modelexplorer/src/org/eclipse/papyrus/views/modelexplorer/handler/DeleteCommandHandler.java
+++ plugins/views/modelexplorer/org.eclipse.papyrus.views.modelexplorer/src/org/eclipse/papyrus/views/modelexplorer/handler/DeleteCommandHandler.java
@@ -8,8 +8,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *
  * 		Yann Tanguy (CEA LIST) yann.tanguy@cea.fr - Initial API and implementation
+ * 		Gabriel Pascual (ALL4TEC) gabriel.pascual@all4tec - Bug 436952
  *
  *****************************************************************************/
 package org.eclipse.papyrus.views.modelexplorer.handler;
@@ -22,6 +22,7 @@ import org.eclipse.core.commands.IHandler;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.command.UnexecutableCommand;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
@@ -30,8 +31,13 @@ import org.eclipse.papyrus.infra.core.sasheditor.contentprovider.IPageManager;
 import org.eclipse.papyrus.infra.core.services.ServiceException;
 import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
 import org.eclipse.papyrus.infra.emf.utils.ServiceUtilsForEObject;
+import org.eclipse.papyrus.infra.services.controlmode.ControlModeManager;
+import org.eclipse.papyrus.infra.services.controlmode.ControlModeRequest;
+import org.eclipse.papyrus.infra.services.controlmode.IControlModeManager;
+import org.eclipse.papyrus.infra.services.controlmode.util.ControlHelper;
 import org.eclipse.papyrus.infra.services.edit.service.ElementEditServiceUtils;
 import org.eclipse.papyrus.infra.services.edit.service.IElementEditService;
+import org.eclipse.papyrus.views.modelexplorer.Activator;
 
 /**
  * Default handler for Delete command used in the ModelExplorer contextual menu.
@@ -105,6 +111,21 @@ public class DeleteCommandHandler extends AbstractCommandHandler implements IHan
 			IElementEditService provider = ElementEditServiceUtils.getCommandProvider(selectedEObject);
 			if (provider == null) {
 				continue;
+			}
+			
+			// Look for uncontrol mode command
+			TransactionalEditingDomain editingDomain = null;
+			try {
+				editingDomain = ServiceUtilsForEObject.getInstance().getTransactionalEditingDomain(selectedEObject);
+			} catch (ServiceException e) {
+				Activator.log.error(e);
+			}
+			if (editingDomain !=null && ControlHelper.isRootControlledObject(selectedEObject)) {
+				ControlModeRequest controlRequest = ControlModeRequest.createUIUncontrolModelRequest(editingDomain, selectedEObject);
+				IControlModeManager controlMng = ControlModeManager.getInstance();
+				ICommand controlCommand = controlMng.getUncontrolCommand(controlRequest);
+
+				gmfCommand =  CompositeCommand.compose(gmfCommand, controlCommand);
 			}
 
 			// Retrieve delete command from the Element Edit service
