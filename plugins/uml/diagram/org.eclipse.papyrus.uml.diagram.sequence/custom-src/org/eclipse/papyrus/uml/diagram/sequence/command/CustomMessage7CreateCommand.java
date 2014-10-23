6823cc99041347f1ad2fb2a370adef2972f556c2
sboyko <serg.boyko2011@gmail.com>
2014-10-23 18:40:12 +0400

    Bug 445271 - [Sequence Diagram] Cannot ceate a MessageCreate from within a Combined Fragment to a place outside the CF
    Change-Id: Ibed7a460f1c2c9c9fef4682fe40d72a4c64194af
    Signed-off-by: sboyko <serg.boyko2011@gmail.com>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/command/CustomMessage7CreateCommand.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/command/CustomMessage7CreateCommand.java
@@ -51,13 +51,9 @@ public class CustomMessage7CreateCommand extends Message7CreateCommand {
 	 */
 	@Override
 	public boolean canExecute() {
+		// #445271: we don't care about CommandHelper.hasValidContainer() 
 		if (!super.canExecute()) {
 			return false;
-		}
-		if (getSource() != null && getTarget() != null) {
-			if (!CommandHelper.hasValidContainer(getRequest())) {
-				return false;
-			}
 		}
 		return MessageConnectionHelper.canExist(MessageSort.ASYNCH_SIGNAL_LITERAL, null, getTarget());
 	}
