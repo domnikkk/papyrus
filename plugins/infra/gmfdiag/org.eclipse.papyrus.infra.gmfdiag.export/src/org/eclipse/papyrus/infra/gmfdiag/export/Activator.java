a6e8b90dfed2346e6949febcb87abe8f9265ac94
Camille Letavernier <camille.letavernier@cea.fr>
2014-11-07 13:12:13 +0100

    [Releng] Fix bundle metadata

--- plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.export/src/org/eclipse/papyrus/infra/gmfdiag/export/Activator.java
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.export/src/org/eclipse/papyrus/infra/gmfdiag/export/Activator.java
@@ -28,7 +28,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.infra.export"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.infra.gmfdiag.export"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
