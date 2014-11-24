b65597e9ce0a4df2a0fb4d3f5d31707227015946
Mickael ADAM <mickael.adam@ALL4TEC.net>
2014-11-24 16:20:38 +0100

    436547: [Diagram] All shapes shall be refactored https://bugs.eclipse.org/bugs/show_bug.cgi?id=436547
    -Adds OneTopLineResizableBorder, a line border on top of the compartment
    which the size can be set. More when the length is not forced, the line
    stays on the figure when there is rounded corner or it's an oval.
    
    CSS properties: lineLength and lineLengthRatio
    
    Change-Id: Ia031564bd3478b9081df8166b217b1acba381052
    Signed-off-by: Mickael ADAM <mickael.adam@ALL4TEC.net>

--- plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/editpolicies/BorderDisplayEditPolicy.java
+++ plugins/infra/gmfdiag/org.eclipse.papyrus.infra.gmfdiag.common/src/org/eclipse/papyrus/infra/gmfdiag/common/editpolicies/BorderDisplayEditPolicy.java
@@ -1,6 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2013 CEA LIST.
- *
+ * Copyright (c) 2010, 2014 CEA LIST and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,8 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
- *  Micka�l ADAM (ALL4TEC) mickael.adam@all4tec.net - set the border to null of the first Compartment of an editPart.
+ *   Mickael ADAM (ALL4TEC) mickael.adam@all4tec.net - Initial API and Implementation
  *
  *****************************************************************************/
 package org.eclipse.papyrus.infra.gmfdiag.common.editpolicies;
@@ -22,7 +20,6 @@ import org.eclipse.core.databinding.observable.IChangeListener;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.draw2d.Border;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
@@ -33,7 +30,6 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.CompartmentEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ITextAwareEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
-import org.eclipse.gmf.runtime.draw2d.ui.figures.OneLineBorder;
 import org.eclipse.gmf.runtime.gef.ui.internal.editpolicies.GraphicalEditPolicyEx;
 import org.eclipse.gmf.runtime.notation.BooleanValueStyle;
 import org.eclipse.gmf.runtime.notation.NotationPackage;
@@ -41,17 +37,33 @@ import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
 import org.eclipse.papyrus.infra.gmfdiag.common.databinding.custom.CustomBooleanStyleObservableValue;
 import org.eclipse.papyrus.infra.gmfdiag.common.editpart.IPapyrusEditPart;
+import org.eclipse.papyrus.infra.gmfdiag.common.figure.OneTopLineResizableBorder;
+import org.eclipse.papyrus.infra.gmfdiag.common.model.NotationUtils;
 
 /**
- * this edit policy has in charge to toggle the border of its edit part's figure
+ * this edit policy has in charge to toggle the border of its edit part's figure.
  */
 public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IChangeListener, NotificationListener {
 
+	/** The default value for the line length ratio of compartment. */
+	private static final String LINE_LENGTH_RATIO_DEFAULT_VALUE = "1.0";
+
+	/** The notation property to define the line length ratio of compartment's topLine. */
+	private static final String LINE_LENGTH_RATIO = "lineLengthRatio";
+
+	/** the default value for the line length of compartment. If the length is set to this default value, the length will be not forced */
+	private static final int DEFAULT_LENGTH_VALUE = -1;
+
+	/** The notation property to define the line length of compartment's topLine. */
+	public static final String LENGTH = "lineLength";
+
+	/** The notation property to display. */
 	public static final String DISPLAY_BORDER = "displayBorder";
 
-	/** key for this edit policy */
+	/** key for this edit policy. */
 	public final static String BORDER_DISPLAY_EDITPOLICY = "BORDER_DISPLAY_EDITPOLICY";
 
+	/** The style observable. */
 	protected IObservableValue styleObservable;
 
 	/**
@@ -69,8 +81,7 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 		styleObservable = new CustomBooleanStyleObservableValue(view, EMFHelper.resolveEditingDomain(view), DISPLAY_BORDER);
 		styleObservable.addChangeListener(this);
 
-		// adds a listener on the view and the element controlled by the
-		// editpart
+		// adds a listener on the view and the element controlled by the editpart
 		getDiagramEventBroker().addNotificationListener(view, this);
 
 		EObject semanticElement = EMFHelper.getEObject(getHost());
@@ -95,16 +106,6 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 		return null;
 	}
 
-
-	/**
-	 *
-	 * @param currentView
-	 * @return the current Style that reperesent the boder
-	 */
-	protected BooleanValueStyle getMaintainSymbolRatioStyle(View currentView) {
-		return (BooleanValueStyle) currentView.getNamedStyle(NotationPackage.eINSTANCE.getBooleanValueStyle(), DISPLAY_BORDER);
-	}
-
 	/**
 	 *
 	 * {@inheritDoc}
@@ -118,21 +119,45 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 		}
 	}
 
+	/**
+	 * Notify changed.
+	 *
+	 * @param notification
+	 *            the notification
+	 * @see org.eclipse.gmf.runtime.diagram.core.listener.NotificationListener#notifyChanged(org.eclipse.emf.common.notify.Notification)
+	 */
 	@Override
 	public void notifyChanged(Notification notification) {
 		refreshBorderDisplay();
 	}
 
+	/**
+	 * Handle change.
+	 *
+	 * @param event
+	 *            the event
+	 * @see org.eclipse.core.databinding.observable.IChangeListener#handleChange(org.eclipse.core.databinding.observable.ChangeEvent)
+	 */
 	@Override
 	public void handleChange(ChangeEvent event) {
 		getHost().refresh();
 	}
 
+	/**
+	 * Refresh.
+	 *
+	 * @see org.eclipse.gmf.runtime.gef.ui.internal.editpolicies.GraphicalEditPolicyEx#refresh()
+	 */
 	@Override
 	public void refresh() {
 		refreshBorderDisplay();
 	}
 
+	/**
+	 * Gets the primary shape.
+	 *
+	 * @return the primary shape
+	 */
 	protected IFigure getPrimaryShape() {
 		EditPart host = getHost();
 		if (host instanceof IPapyrusEditPart) {
@@ -143,6 +168,11 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 		return getHostFigure();
 	}
 
+	/**
+	 * Gets the notation view.
+	 *
+	 * @return the notation view
+	 */
 	protected View getNotationView() {
 		EditPart host = getHost();
 		if (host instanceof org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart) {
@@ -153,8 +183,12 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 		return view;
 	}
 
+	/** The default border. */
 	protected Border defaultBorder;
 
+	/**
+	 * Refresh border display.
+	 */
 	protected void refreshBorderDisplay() {
 		View view = getNotationView();
 		if (view == null) {
@@ -169,38 +203,41 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 				defaultBorder = getPrimaryShape().getBorder();
 			}
 			getPrimaryShape().setBorder(null);
-		} else {// if diplayBorder
-			if (defaultBorder != null) {
-				getPrimaryShape().setBorder(defaultBorder);
-			}
-			defaultBorder = null;
-
-			// If the shape is a compartment and it's the first one, set border to null.
+		} else {
+			// if diplayBorder
+			// If the shape is a compartment
 			if (getPrimaryShape() instanceof ResizableCompartmentFigure) {
+				if (!(defaultBorder instanceof OneTopLineResizableBorder)) {
+					defaultBorder = new OneTopLineResizableBorder(2);
+				}
+				// Set OneLineResizableBorder as border
+				getPrimaryShape().setBorder(defaultBorder);
 
-				// Get all visible compartment
+				// If a length or a length ratio is defined in notation or CSS
+				((OneTopLineResizableBorder) defaultBorder).setLength(NotationUtils.getIntValue(view, LENGTH, DEFAULT_LENGTH_VALUE));
+				((OneTopLineResizableBorder) defaultBorder).setLengthRatio(new Float(NotationUtils.getStringValue(view, LINE_LENGTH_RATIO, LINE_LENGTH_RATIO_DEFAULT_VALUE)));
+
+				// If it's the first one, set border to null.
 				final List<View> allVisibleCompartments = getAllVisibleCompartments(view, getHost().getParent());
 				// If the view is the first compartment set border to null
 				if (allVisibleCompartments.size() > 0 && allVisibleCompartments.get(0).equals(view)) {
 					getPrimaryShape().setBorder(null);
-				} else {
-					if (getPrimaryShape().getBorder() == null) {
-						getPrimaryShape().setBorder(new OneLineBorder(2, PositionConstants.TOP));
-					}
+				}
+			} else {
+				if (defaultBorder != null) {
+					getPrimaryShape().setBorder(defaultBorder);
 				}
 			}
 		}
 	}
 
 	/**
-	 * Returns all the displayed compartments for the EditPart
-	 * 
+	 * Returns all the displayed compartments for the EditPart.
+	 *
 	 * @param view
-	 * 
+	 *            the view
 	 * @param editpart
 	 *            an editpart
-	 * @param includeTitleCompartment
-	 *            if <code>true</code> we return the title compartment too
 	 * @return all the displayed compartments for the EditPart
 	 */
 	public static List<View> getAllVisibleCompartments(View view, EditPart editpart) {
@@ -227,6 +264,13 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 		return visibleCompartments;
 	}
 
+	/**
+	 * Find display border style.
+	 *
+	 * @param view
+	 *            the view
+	 * @return the boolean value style
+	 */
 	private BooleanValueStyle findDisplayBorderStyle(View view) {
 		View parentView = view;
 		while (parentView.getElement() == view.getElement()) {
@@ -245,5 +289,4 @@ public class BorderDisplayEditPolicy extends GraphicalEditPolicyEx implements IC
 
 		return null;
 	}
-
 }
