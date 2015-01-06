4e2b7a5e1e22cc5b32d10d8e89b55219d0d0a4b0
Camille Letavernier <camille.letavernier@cea.fr>
2015-01-06 18:34:42 +0100

    455001: [Model Import] Log execution time information https://bugs.eclipse.org/bugs/show_bug.cgi?id=455001
    Performance improvements based on initial log results: 
    
    - Load only the main model resources (instead of resolveAll)
    - Add a Pool of executors to allow real multi-thread execution (The
    number of threads is limited by the number of available executors)

---
+++ extraplugins/migration/org.eclipse.papyrus.migration.rsa/src/org/eclipse/papyrus/migration/rsa/transformation/ExecutorsPool.java
@@ -0,0 +1,168 @@
+/*****************************************************************************
+ * Copyright (c) 2014 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.migration.rsa.transformation;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.m2m.qvt.oml.TransformationExecutor;
+import org.eclipse.papyrus.migration.rsa.Activator;
+
+/**
+ * Thread safe pool of Transformation Executors
+ *
+ * @author Camille Letavernier
+ *
+ */
+public class ExecutorsPool {
+
+	// The cache can be used to increase performances (For small and medium sized models, most of the execution time is spent in loading the transformation)
+	// Warning: using the cache prevents dynamic transformations (i.e. it should not be used in Debug Mode)
+	protected boolean cacheTransformations = true;
+
+	private final int poolSize;
+
+	public ExecutorsPool(int size) {
+		this.poolSize = size;
+	}
+
+	private final Map<URI, Pool> executors = new HashMap<URI, Pool>();
+
+	private final Map<TransformationExecutor, URI> executorsURIs = new HashMap<TransformationExecutor, URI>();
+
+	/**
+	 * Gets an available executor for the given transformation URI. Creates it if needed,
+	 * and we haven't reached the poolSize for this transformationURI
+	 *
+	 * Transformation executor needs to be released after usage
+	 *
+	 * @param transformationURI
+	 * @return
+	 */
+	public TransformationExecutor getExecutor(URI transformationURI) {
+		if (!cacheTransformations) {
+			return new Pool(transformationURI).createExecutor();
+		}
+
+		synchronized (executors) {
+			if (!executors.containsKey(transformationURI)) {
+				executors.put(transformationURI, new Pool(transformationURI));
+			}
+		}
+
+		Pool pool = executors.get(transformationURI);
+		return pool.getExecutor();
+	}
+
+	/**
+	 * Preloads all instances for the given URI
+	 *
+	 * @param transformationURI
+	 * @return
+	 */
+	public synchronized IStatus preLoad(URI transformationURI) {
+		// Don't preload if there is no cache
+		if (!cacheTransformations) {
+			return Status.OK_STATUS;
+		}
+
+		synchronized (this) {
+			if (!executors.containsKey(transformationURI)) {
+				Pool pool = new Pool(transformationURI);
+				executors.put(transformationURI, pool);
+				pool.preload();
+			}
+		}
+
+		return Status.OK_STATUS;
+	}
+
+	/**
+	 * Releases a transformation executor after usage
+	 *
+	 * @param executor
+	 */
+	public void releaseExecutor(TransformationExecutor executor) {
+		URI transformationURI = executorsURIs.get(executor);
+		Pool pool = executors.get(transformationURI);
+		pool.release(executor);
+	}
+
+	private class Pool {
+		private List<TransformationExecutor> allExecutors = new LinkedList<TransformationExecutor>();
+
+		private List<TransformationExecutor> busyExecutors = new LinkedList<TransformationExecutor>();
+
+		private final URI transformationURI;
+
+		public Pool(URI transformationURI) {
+			this.transformationURI = transformationURI;
+		}
+
+		public void preload() {
+			for (int i = 0; i < poolSize; i++) {
+				createExecutor();
+			}
+			busyExecutors.clear();
+		}
+
+		public synchronized void release(TransformationExecutor executor) {
+			busyExecutors.remove(executor);
+		}
+
+		public TransformationExecutor getExecutor() {
+			while (true) {
+				synchronized (this) {
+					for (TransformationExecutor executor : allExecutors) {
+						if (isAvailable(executor)) {
+							return getExecutor(executor);
+						}
+					}
+
+					if (allExecutors.size() < poolSize) {
+						return createExecutor();
+					}
+				}
+
+				try {
+					Thread.sleep(25);
+				} catch (InterruptedException ex) {
+					Activator.log.error(ex);
+					return null;
+				}
+			}
+		}
+
+		private boolean isAvailable(TransformationExecutor executor) {
+			return !busyExecutors.contains(executor);
+		}
+
+		private TransformationExecutor getExecutor(TransformationExecutor executor) {
+			busyExecutors.add(executor);
+			return executor;
+		}
+
+		private TransformationExecutor createExecutor() {
+			TransformationExecutor executor = new TransformationExecutor(transformationURI);
+			executor.loadTransformation();
+			allExecutors.add(executor);
+			executorsURIs.put(executor, transformationURI);
+			return getExecutor(executor);
+		}
+
+	}
+}
