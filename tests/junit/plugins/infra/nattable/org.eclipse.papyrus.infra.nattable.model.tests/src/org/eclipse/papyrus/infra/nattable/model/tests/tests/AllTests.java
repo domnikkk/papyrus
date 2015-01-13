1f9c23a71af419d39b41b5a0e93a780c5ed4e8c1
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2015-01-13 15:56:32 +0100

    439888: [Table] Papyrus must provides hierarchical tables  + fix tests + add JUnit tests for requirement table

--- tests/junit/plugins/infra/nattable/org.eclipse.papyrus.infra.nattable.model.tests/src/org/eclipse/papyrus/infra/nattable/model/tests/tests/AllTests.java
+++ tests/junit/plugins/infra/nattable/org.eclipse.papyrus.infra.nattable.model.tests/src/org/eclipse/papyrus/infra/nattable/model/tests/tests/AllTests.java
@@ -1,25 +1,26 @@
 /*****************************************************************************
- * Copyright (c) 2014 CEA LIST.
+ * Copyright (c) 2012 CEA LIST.
  *
- *    
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
- *
+ *  Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
  *****************************************************************************/
-package org.eclipse.papyrus.infra.nattable.tests.tests;
+package org.eclipse.papyrus.infra.nattable.model.tests.tests;
 
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
 @RunWith(Suite.class)
-@SuiteClasses({ TableSelectionProviderTest.class , PasteTreeUtils_Tests.class})
+@SuiteClasses({ StyledElementTests.class
+
+})
 public class AllTests {
-	// JUnit 4 test suite
+	//Test Suite
 }
