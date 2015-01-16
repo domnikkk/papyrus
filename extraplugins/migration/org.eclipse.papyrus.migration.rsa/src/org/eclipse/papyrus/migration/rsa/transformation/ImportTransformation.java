b3cef8ce7069319fd3fb0c9e0d4d902fce94e002
Camille Letavernier <camille.letavernier@cea.fr>
2015-01-16 10:21:23 +0100

    457668: [Model Import] Dangling references when saving resources https://bugs.eclipse.org/bugs/show_bug.cgi?id=457668
    - Add the proper save options to all resources
    - Add log information when save errors occur

--- extraplugins/migration/org.eclipse.papyrus.migration.rsa/src/org/eclipse/papyrus/migration/rsa/transformation/ImportTransformation.java
+++ extraplugins/migration/org.eclipse.papyrus.migration.rsa/src/org/eclipse/papyrus/migration/rsa/transformation/ImportTransformation.java
@@ -534,6 +534,7 @@ public class ImportTransformation {
 					resource.save(null);
 				} catch (Exception ex) {
 					Activator.log.error(ex);
+					generationStatus.add(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "An exception occurred during save", ex));
 				}
 			}
 		}
@@ -857,6 +858,12 @@ public class ImportTransformation {
 		}
 
 		handleFragmentStereotypes(umlResource, fragmentUMLResources);
+
+		for (Resource resource : result) {
+			if (resource instanceof XMIResource) {
+				configureResource((XMIResource) resource);
+			}
+		}
 
 		return result;
 	}
